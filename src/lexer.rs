eb8949c16e6cfa399d98d7835f461f2bf0d0d28a
Nicolas Alvarado <naav972@gmail.com>
2023-07-24 21:30:01 -0500

    un primer avance

--- src/lexer.rs
+++ src/lexer.rs
@@ -12,18 +12,48 @@ struct Token {
 fn tokenize<'a>(source: &str, tlist: &'a mut Vec<Token>) -> &'a mut Vec<Token> {
     let mut slstring = source.replace("\n", "");
     slstring = slstring.replace("\t", "");
-    slstring = slstring.replace(" ", "");
-    println!("{}", slstring);
-    let chars = slstring.split("");
-    for l in chars {
-        println!("{}", l);
+    slstring = slstring.replace("    ", "");
+    slstring = slstring.replace("<", " < ");
+    slstring = slstring.replace(">", " > ");
+    slstring = slstring.replace("!", " ! ");
+    slstring = slstring.replace("=", " = ");
+    slstring = slstring.replace("/", " / ");
+
+    slstring = slstring.replace("  ", " ");
+
+    let chars = slstring.split(" ");
+    let vchar: Vec<&str> = chars.collect();
+    let mut i = 0;
+    while i < vchar.len() {
+        if vchar[i] == "<" {
+            if vchar[i+1] == "/" {
+                println!("close tag!");
+            }
+            else if vchar[i+1] == "!" {
+                if vchar[i+2] == "DOCTYPE" {
+                    println!("DOCTYPE");
+                }
+                else if vchar[i+2].starts_with("-") {
+                    println!("comment");
+                }
+                else {
+                    println!("ERROR: Whas expecting DOCTYPE tag or comment but got {}", vchar[i+2]);
+                    break;
+                }
+            }
+            else {
+                println!("open tag!");
+            }
+        }
+        i = i + 1;
     }
     tlist
 }
 
 pub fn main() {
     let mut tokens: Vec<Token> = Vec::new();
-    let html = "<html>
+    let html = "<!DOCTYPE> <!-- doctye --> <!fregrw
+        <html>
         </html>";
     tokenize(html, &mut tokens);
 }
