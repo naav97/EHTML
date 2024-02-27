2957dcce382c85c382646c9b17e037c4403cb575
Nicolas Alvarado <naav972@gmail.com>
2024-02-26 21:31:13 -0500

    init vars

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 mod html;
 mod seo;
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 
+fn populate_vars(data: &str) -> HashMap<String, String> {
+    let mut vars: HashMap<String, String> = HashMap::new();
+    let data = &data.replace(">", ">`");
+    let data = if data.ends_with("`") { data[..data.len() - 1].to_string() } else { data.to_string() };
+    let decl: Vec<&str> = data.split("`").collect();
+    for dec in decl {
+        if dec.starts_with("<let ") {
+            println!("{}", dec);
+        }
+        else {
+            println!("ERROR: not a declaration ({})", dec);
+        }
+    }
+    return vars;
+}
+
 fn main() {
-    //lexer::main();
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         println!("File not provided");
@@ -16,9 +32,11 @@ fn main() {
         println!("HTML file read succesfull");
         let mut trimmed = html_content.replace("\t", "");
         trimmed = trimmed.replace("\n", "");
-        if html::validate_html_structure(&trimmed) {
+        let parts: Vec<&str> = trimmed.split("<endvars/>").collect();
+        let vars = populate_vars(parts[0]);
+        if html::validate_html_structure(&parts[1]) {
             println!("HTML structure ok!");
-            if seo::check_seo(&trimmed) {
+            if seo::check_seo(&parts[1]) {
                 println!("SEO ok!");
             }
             else {
